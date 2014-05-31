80950db8ff4e2efa5527c7e8f61e1e11a2b0d5ed
Jared De Blander <jared@deblander.org>
2014-05-31 12:36:37 -0400

    new links

--- index.php
+++ index.php
@@ -48,17 +48,15 @@ webrtc.on('readyToCall', function () {
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.php">Start Bootstrap</a>
+                <a class="navbar-brand" href="/">Home</a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse navbar-ex1-collapse">
                 <ul class="nav navbar-nav">
-                    <li><a href="#about">About</a>
+                    <li><a href="/random">New Random Room</a>
                     </li>
-                    <li><a href="#services">Services</a>
-                    </li>
-                    <li><a href="#contact">Contact</a>
+                    <li><a href="/about">About</a>
                     </li>
                 </ul>
             </div>
